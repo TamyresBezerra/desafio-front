0d9621a0f733c9058520342e233777a1d64a128e
Tamyres <tamyresbezerraa@gmail.com>
2019-05-20 20:35:53 -0300

    Primeiro commit fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,3 @@
 export const environment = {
-  production: true,
-  configFirebase: {
-    apiKey: "AIzaSyBEQMXbtkDV3TIZvVNo307THriz9sl2VeA",
-    authDomain: "cliente-oculto-d5901.firebaseapp.com",
-    databaseURL: "https://cliente-oculto-d5901.firebaseio.com",
-    projectId: "cliente-oculto-d5901",
-    storageBucket: "cliente-oculto-d5901.appspot.com",
-    messagingSenderId: "1067508396429"
-  }
+  production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,15 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  configFirebase: {
-    apiKey: "AIzaSyBEQMXbtkDV3TIZvVNo307THriz9sl2VeA",
-    authDomain: "cliente-oculto-d5901.firebaseapp.com",
-    databaseURL: "https://cliente-oculto-d5901.firebaseio.com",
-    projectId: "cliente-oculto-d5901",
-    storageBucket: "cliente-oculto-d5901.appspot.com",
-    messagingSenderId: "1067508396429"
-  }
+  production: false
 };
 
 /*
